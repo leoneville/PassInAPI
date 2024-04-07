44cb0575510227b8db06b5ee3d3f177949469450
leoneville <leoneville_@hotmail.com>
2024-04-07 11:11:19 -0300

    improve: addin try in events repository

--- src/models/repository/events_repository.py
+++ src/models/repository/events_repository.py
@@ -1,24 +1,39 @@
 from typing import Dict, Optional
+
+from sqlalchemy.exc import IntegrityError, NoResultFound
+
 from src.models.settings.connection import db_connection_handler
 from src.models.entities import Events
 
 class EventsRepository:
     def insert_event(self, events_info: Dict) -> Dict:
         with db_connection_handler as db:
-            event = Events(
-                id=events_info.get("uuid"),
-                title=events_info.get("title"),
-                details=events_info.get("details"),
-                slug=events_info.get("slug"),
-                maximum_attendees=events_info.get("maximum_attendees"),
-            )
-            db.session.add(event)
-            db.session.commit()
+            try:
+                event = Events(
+                    id=events_info.get("uuid"),
+                    title=events_info.get("title"),
+                    details=events_info.get("details"),
+                    slug=events_info.get("slug"),
+                    maximum_attendees=events_info.get("maximum_attendees"),
+                )
+                db.session.add(event)
+                db.session.commit()
 
-            return events_info
+                return events_info
+            
+            except IntegrityError:
+                raise Exception("Evento já cadastrado!")
+
+            except Exception as exception:
+                db.session.rollback()
+                raise exception
         
     def get_event_by_id(self, event_id: str) -> Optional[Events]:
         with db_connection_handler as db:
-            event = db.session.query(Events).filter_by(id=event_id).first()
+            try:
+                event = db.session.query(Events).filter_by(id=event_id).one()
 
-            return event
+                return event
+            
+            except NoResultFound:
+                return None
--- src/models/repository/events_repository_test.py
+++ src/models/repository/events_repository_test.py
@@ -1,3 +1,5 @@
+from uuid import uuid4
+
 import pytest
 from src.models.settings.connection import db_connection_handler as db
 from .events_repository import EventsRepository
@@ -7,7 +9,7 @@ db.connect_to_db()
 @pytest.mark.skip(reason="Novo registro em banco de dados")
 def test_insert_event():
     event = {
-        "uuid": "meu-uuid-2",
+        "uuid": uuid4().hex,
         "title": "meu titulo",
         "slug": "meu-slug-2",
         "maximum_attendees": 20
@@ -17,9 +19,11 @@ def test_insert_event():
     response = events_repository.insert_event(event)
     print(response)
 
+@pytest.mark.skip(reason="Não necessita")
 def test_get_event_by_id():
     event_id = "meu-uuid-2"
 
     events_repository = EventsRepository()
     response = events_repository.get_event_by_id(event_id)
-    print(response.as_dict())
+    print(response)
+    print(response.title)
