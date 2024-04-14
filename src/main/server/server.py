d868bcf22be5aa4225b190cce7dfc98bfe5324ff
leoneville <leoneville_@hotmail.com>
2024-04-14 10:07:18 -0300

    feat: Implementing AttendeesHandle with attendee badge route

--- src/main/server/server.py
+++ src/main/server/server.py
@@ -1,3 +1,5 @@
+from src.main.routes.attendees_routes import attendees_route_bp
+from src.main.routes.event_routes import event_route_bp
 from flask import Flask
 from flask_cors import CORS
 from src.models.settings.connection import db_connection_handler
@@ -7,5 +9,6 @@ db_connection_handler.connect_to_db()
 app = Flask(__name__)
 CORS(app)
 
-from src.main.routes.event_routes import event_route_bp
+
 app.register_blueprint(event_route_bp)
+app.register_blueprint(attendees_route_bp)
--- src/models/repository/attendees_repository.py
+++ src/models/repository/attendees_repository.py
@@ -4,6 +4,7 @@ from src.models.settings.connection import db_connection_handler
 from src.models.entities import Attendees, Events
 from sqlalchemy.exc import NoResultFound, IntegrityError
 
+
 class AttendeesRepository:
 
     def insert_attendee(self, attendee_info: Dict) -> Dict:
@@ -24,21 +25,21 @@ class AttendeesRepository:
             except Exception as exception:
                 db.session.rollback()
                 raise exception
-            
 
     def get_attendee_badge_by_id(self, attendee_id: str) -> Optional[Attendees]:
         with db_connection_handler as db:
             try:
                 attendee = (
                     db.session
-                        .query(Attendees)
-                        .join(Events, Events.id == Attendees.event_id)
-                        .filter(Attendees.id == attendee_id)
-                        .with_entities(
-                            Attendees.id,
-                            Attendees.email,
-                            Events.title
-                        ).one()
+                    .query(Attendees)
+                    .join(Events, Events.id == Attendees.event_id)
+                    .filter(Attendees.id == attendee_id)
+                    .with_entities(
+                        Attendees.id,
+                        Attendees.name,
+                        Attendees.email,
+                        Events.title
+                    ).one()
                 )
                 return attendee
 
